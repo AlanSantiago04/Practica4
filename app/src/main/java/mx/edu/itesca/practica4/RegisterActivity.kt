aa9052205331655bb7338b2022b9f50ebe6d8c55
AlanSantiago04 <alansantiagovlz61@gmail.com>
2026-02-12 09:51:24 -0700

    segunda version

--- app/src/main/java/mx/edu/itesca/practica4/RegisterActivity.kt
+++ app/src/main/java/mx/edu/itesca/practica4/RegisterActivity.kt
@@ -13,14 +13,15 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        setContentView(R.layout.activity_register)
+
         val button: Button = findViewById(R.id.btn_sign_in)
 
-        button.setOnClickListener{
-            var intent = Intent(this, MenuActivity::class.java)
+        button.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
 
-        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
